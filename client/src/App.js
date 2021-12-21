8096da2483fda23046975a1c41d4a211e0c5200e
spcmnd <spcmndd@protonmail.com>
2021-12-21 22:57:49 +0100

    client: WIP - Display task list

--- client/src/App.js
+++ client/src/App.js
@@ -4,35 +4,54 @@ import "./App.css";
 import TodoList from "./contract/TodoList.json";
 
 function App() {
+  const [contract, setContract] = useState();
   const [account, setAccount] = useState();
   const [balance, setBalance] = useState();
-
-  const [contract, setContract] = useState();
   const [taskCount, setTaskCount] = useState();
+  const [tasks, setTasks] = useState([]);
 
-  useEffect(() => {
-    async function load() {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      await provider.send("eth_requestAccounts", []);
-      const signer = provider.getSigner();
+  useEffect(() => loadBlockchainData());
 
-      setAccount(await signer.getAddress());
-      setBalance(
-        ethers.utils.formatEther((await signer.getBalance()).toString())
-      );
+  async function loadBlockchainData() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    const address = await signer.getAddress();
+    setAccount(address);
+    const balance = ethers.utils.formatEther(
+      (await signer.getBalance()).toString()
+    );
+    setBalance(balance);
+    const todoList = new ethers.Contract(
+      process.env.REACT_APP_SMART_CONTRACT_ADDRESS,
+      TodoList.abi,
+      signer
+    );
+    setContract(todoList);
+    const count = (await todoList.taskCount()).toNumber();
+    setTaskCount(count);
+    loadTasks();
+  }
 
-      const todoList = new ethers.Contract(
-        process.env.REACT_APP_SMART_CONTRACT_ADDRESS,
-        TodoList.abi,
-        signer
-      );
+  async function loadTasks() {
+    const loadedTasks = [];
 
-      setContract(todoList);
-      setTaskCount((await todoList.taskCount()).toNumber());
+    for (let index = 1; index <= taskCount; index++) {
+      const task = await contract.tasks(index);
+      loadedTasks.push(task);
     }
 
-    load();
-  }, []);
+    setTasks(loadedTasks);
+  }
+
+  async function handleFormSubmit(event) {
+    event.preventDefault();
+    const transactionResponse = await contract.createTask(
+      event?.target?.content?.value
+    );
+    await transactionResponse.wait();
+    loadTasks();
+  }
 
   return (
     <div className="App">
@@ -42,6 +61,19 @@ function App() {
       <p>Current contract is: {contract?.address}</p>
       <hr />
       <p>Task count: {taskCount}</p>
+      <h2>Add a new task</h2>
+      <form onSubmit={handleFormSubmit}>
+        <input type="text" name="content" />
+        <button type="submit">Submit</button>
+      </form>
+      <div className="tasks">
+        {tasks.map((t) => (
+          <div key={t.id.toNumber()} className="task">
+            {t.id.toNumber()} - {t.content} -{" "}
+            <input type="checkbox" checked={t.id.completed} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
